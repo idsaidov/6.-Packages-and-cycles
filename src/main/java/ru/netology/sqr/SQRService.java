d64c4d5dbe964dd1a79c2b97e593463f79933ffb
idsaidov <id.saidov@yandex.ru>
2022-08-19 12:29:53 +0300

    Commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,11 +2,12 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int calcSqrt(int lowerBound, int upperBound) {
+        int cont = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= lowerBound && i * i <= upperBound) {
-                return i;
+                cont++;
             }
         }
-        return -1;
+        return cont;
     }
 }
